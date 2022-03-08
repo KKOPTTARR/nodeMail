33c5d2a90378b99a3b6c3820638e99e79aa0f957
林嘉康 <Kkopttarr@habby.com>
2022-03-08 18:20:41 +0800

    update url

--- controller/qqController.js
+++ controller/qqController.js
@@ -2,7 +2,7 @@ const QQSendMail = require('../Service/qqService');
 
 //通过预设的QQ邮箱发送
 async function QQSend() {
-    await QQSendMail('dbfiip@163.com','测试', 'Hi 小波,这是一封');
+    await QQSendMail('18161963716@163.com','测试', 'Hi 点击一下带你去一个好玩的地方 www.baidu.com');
 }
 
 QQSend();
